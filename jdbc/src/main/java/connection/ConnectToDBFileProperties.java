e103ade1cc38c09e97bb76050edbc0f6d2371d17
Kovteba Dima <kovteba@gmail.com>
2020-04-18 10:49:06 +0300

    Add ConnectToDBFileProperties

---
+++ jdbc/src/main/java/connection/ConnectToDBFileProperties.java
@@ -0,0 +1,43 @@
+package connection;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class ConnectToDBFileProperties {
+
+    private static final Logger LOG = Logger.getLogger(Connect.class.getName());
+
+    private static final Properties PROPERTIES = new Properties();
+
+    public static Connection connect() {
+
+        try {
+
+            FileInputStream fileWithPropertiesForDB
+                    = new FileInputStream("jdbc/src/main/resources/config.db.properties");
+
+            PROPERTIES.load(fileWithPropertiesForDB);
+            String url = PROPERTIES.getProperty("db.url");
+
+            Properties properties = new Properties();
+            properties.setProperty("user", PROPERTIES.getProperty("db.login"));
+            properties.setProperty("password", PROPERTIES.getProperty("db.password"));
+            properties.setProperty("serverTimezone", PROPERTIES.getProperty("db.serverTimeZone"));
+
+            Connection connection = DriverManager.getConnection(url, properties);
+            LOG.info("Connection to Store DB successfully!");
+
+            return connection;
+
+        } catch (Exception ex) {
+            LOG.warning("Connection failed..." + ex);
+        }
+
+        return null;
+
+    }
+
+}
